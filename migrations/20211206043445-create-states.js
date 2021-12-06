097b5d99612dfdbd53e8dd4e2cd7a910d68c2361
Dan Smestad <hockeydan25@gmail.com>
2021-12-05 23:02:53 -0600

    add in database setup and built array with states built to db.

---
+++ migrations/20211206043445-create-states.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('States', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      visited: {
+        type: Sequelize.BOOLEAN,
+        default: false
+      },
+      lat: {
+        type: Sequelize.FLOAT
+      },
+      lon: {
+        type: Sequelize.FLOAT
+      },
+      zoom: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {//objects and properties
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('States');
+  }
+};
